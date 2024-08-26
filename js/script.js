836af182187cdf747eb355455281326a8e2e0d31
saifgame3 <saifgamevpv@gmail.com>
2024-08-26 17:08:46 +0200

    FINALLY >:(

---
+++ js/script.js
@@ -0,0 +1,18 @@
+ var heart =  document.getElementById('heart1')
+ heart.addEventListener('click',function(){
+         heart.style.color = 'red'
+
+
+});
+var heartb =  document.getElementById('heart2')
+heartb.addEventListener('click',function(){
+        heartb.style.color = 'red'
+
+
+});
+var hearta =  document.getElementById('heart3')
+hearta.addEventListener('click',function(){
+        hearta.style.color = 'red'
+
+
+});
